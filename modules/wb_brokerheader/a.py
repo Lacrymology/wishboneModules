f61520f9d3139b2ebde87a503fb46aa0b08de522
root <root@sandbox.(none)>
2013-02-10 22:20:34 +0000

    not required

--- modules/wb_brokerheader/a.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from pkg_resources import iter_entry_points
-
-
-for object in iter_entry_points(group='wishbone.module', name=None):
-    kak=object.load()
-    
-meuh = kak()
