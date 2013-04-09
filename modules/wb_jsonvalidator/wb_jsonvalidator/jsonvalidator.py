4ea1c32edfbe22ab27e78027c00c0a1f2a62ebf0
Jelle Smet <jelle.smet@tomtom.com>
2013-04-09 21:57:41 +0200

    trailing space

--- modules/wb_jsonvalidator/wb_jsonvalidator/jsonvalidator.py
+++ modules/wb_jsonvalidator/wb_jsonvalidator/jsonvalidator.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 #
 #       jsonvalidator.py
-#       
+#
 #       Copyright 2013 Jelle Smet development@smetj.net
-#       
+#
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
 #       the Free Software Foundation; either version 3 of the License, or
 #       (at your option) any later version.
-#       
+#
 #       This program is distributed in the hope that it will be useful,
 #       but WITHOUT ANY WARRANTY; without even the implied warranty of
 #       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -18,8 +18,8 @@
 #       along with this program; if not, write to the Free Software
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #       MA 02110-1301, USA.
-#       
-#       
+#
+#
 
 import json
 from jsonschema import Validator
@@ -28,23 +28,23 @@ from wishbone.toolkit import PrimitiveActor, TimeFunctions
 
 class JSONValidator(PrimitiveActor, TimeFunctions):
     '''**A Wishbone module which verifies JSON data against a validator schema loaded from file.**
-    
+
     Events consumed from the inbox queue are verified against a Validator schema.  When the event is not a valid JSON document
     or when it doesn't match your predifined Validator schema, it is dropped.
 
-    Parameters:        
-    
-        - name (str):       The instance name when initiated.    
+    Parameters:
+
+        - name (str):       The instance name when initiated.
         - schema (str):     The location and filename of the schema to load.  The schema should follow http://json-schema.org/ specifications.
         - convert (bool):   When True it will aditionally convert the incoming JSON string to a Python object.
-        
-        
-    Queues:        
+
+
+    Queues:
 
         - inbox:    Incoming events.
         - outbox:   Outgoing events.
-    '''    
-    
+    '''
+
     def __init__(self, name, schema=False, convert=False):
         PrimitiveActor.__init__(self, name)
         self.name = name
@@ -65,7 +65,7 @@ class JSONValidator(PrimitiveActor, TimeFunctions):
 
     def consume(self, message):
         '''Executed for each incoming message.'''
-        
+
         try:
             data = json.loads(message["data"])
             self.validate(data)
@@ -74,7 +74,7 @@ class JSONValidator(PrimitiveActor, TimeFunctions):
             self.putData(message)
         except Exception as err:
             self.logging.warning('Invalid data received and purged. Reason: %s' % (err))
-    
+
     @TimeFunctions.do
     def validate(self,data):
         '''Validates data against the JSON schema.'''
